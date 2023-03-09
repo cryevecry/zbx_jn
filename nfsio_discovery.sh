8ac3b1938d9b865f17f8c1663bbef3eaf16aebf5
cryevecry <110468995+cryevecry@users.noreply.github.com>
2023-03-09 13:45:33 +0300

    Update nfsio_discovery.sh

--- nfsio_discovery.sh
+++ nfsio_discovery.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-# ============================================================
-# Author: Dmitry Malinin 
-# E.mail: dmitry@malinin.com
-# Blog: http://zabbix.guru
-# Filename: nfsio_discovery.sh
-# Modified: 2018-01-10 
-# Description: Discovery NFS mountpoints.
-#
-# Parameters:
-#
-# NONE
-#
-# ===========================================================
 
 array="$(findmnt -lo target -n -t nfs,nfs4)"
 
