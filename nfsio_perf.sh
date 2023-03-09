01bf7cc6f44b1d8ddaa69b057303b2d83d01dd7a
cryevecry <110468995+cryevecry@users.noreply.github.com>
2023-03-09 13:45:56 +0300

    Update nfsio_perf.sh

--- nfsio_perf.sh
+++ nfsio_perf.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 # ============================================================
-# Author: Dmitry Malinin
-# E.mail: dmitry@malinin.com
-# Blog: http://zabbix.guru
-# Filename: nfsio_discovery.sh
-# Modified: 2018-01-10
-# Description: Discovery NFS mountpoints.
-#
 # Parameters:
 #
 # nfsio-perf <mountpoint> <metric>
